c94dee6a8442e4f214f8681f2902623fe14bbeac
taylorreichner <taylorreichner@yahoo.com>
2021-05-11 11:34:43 -0700

    jsx color picker

---
+++ src/containers/ColorPicker.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Colors from '../components/app/display/Colors'
+
+export default class ColorPicker extends Component {
+      state = {
+          color: '#FF0000'
+      }
+
+      random = () => {
+          const variety = [
+              '#ff1000',
+              '#ff2000',
+              '#ff3000'
+          ]
+          return variety[Math.floor(Math.random() * variety.length)]
+      }
+    
+}
+  
